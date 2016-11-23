81a20d30f9e045989c044b32982ebf2dacecec16
Andrew Brandt <brandt.andrew89@gmail.com>
2016-11-23 10:43:18 -0700

    Commented Q005 and cleaned up code

--- Q005.py
+++ Q005.py
@@ -6,19 +6,27 @@
 #printString: to print the string in upper case.
 #Also please include simple test function to test the class methods.
 
+# Create a class named StringHandling
 class StringHandling(object):
+	# The init will initialize a string variable in the class
 	def __init__(self):
 		self.string = ""
+	# getString(self) will read the input from the user
 	def getString(self):
 		print "Enter a string:"
 		self.string = raw_input()
+	# printString(self) will print the string in uppercase
 	def printString(self):
 		print self.string.upper()
 
-def test(StringHandling):
-	testingClass = StringHandling
+# Define a test method that expects a StringHandling class type
+def test(testingClass):
+	# Call the getString method
 	testingClass.getString()
+	# Call the printString method
 	testingClass.printString()
 	
+# Create a test object called testObject
 testObject = StringHandling()
+# Test the testObject with the test method
 test(testObject)
